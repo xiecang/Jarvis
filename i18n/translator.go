acfb3ea1211ac9dd14522037f0b09aaddc93c29a
xie_cang <xie_cang@163.com>
2023-12-24 15:48:22 +0800

    feat: add i18n

---
+++ i18n/translator.go
@@ -0,0 +1,37 @@
+package i18n
+
+import "strings"
+
+const (
+	LanguageChinese            Language = "zh_CN"
+	LanguageChineseTraditional Language = "zh_TW"
+	LanguageEnglish            Language = "en_US"
+	LanguageGerman             Language = "de_DE"
+	LanguageSpanish            Language = "es_ES"
+	LanguageFrench             Language = "fr_FR"
+	LanguageItalian            Language = "it_IT"
+	LanguageJapanese           Language = "ja_JP"
+	LanguageKorean             Language = "ko_KR"
+	LanguagePortuguese         Language = "pt_PT"
+	LanguageRussian            Language = "ru_RU"
+	LanguageVietnamese         Language = "vi_VN"
+
+	DefaultLanguage = LanguageEnglish
+)
+
+type Language string
+
+// Abbr get language type abbreviation
+func (t Language) Abbr() string {
+	s := string(t)
+	if idx := strings.Index(s, "_"); idx > 0 {
+		return s[0:idx]
+	}
+	return s
+}
+
+type Translator interface {
+	Tr(lang Language, key string) string
+	TrWithData(lang Language, key string, templateData any) string
+	Dump(lang Language) ([]byte, error)
+}
